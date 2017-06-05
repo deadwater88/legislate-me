06ae50de0ca0419403412a28acb5a9a9f39b685a
David Wong <david.wong.cal@gmail.com>
2017-06-04 20:44:07 -0700

    setup bill model

--- index.android.js
+++ index.android.js
@@ -17,10 +17,10 @@ export default class legislate_me extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to LEGISLATE ME!!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          HI SUNNY AND CHRISTINE! To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
--- index.ios.js
+++ index.ios.js
@@ -17,10 +17,10 @@ export default class legislate_me extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome LEGISLATE ME!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, LOGIN index.ios.js
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
