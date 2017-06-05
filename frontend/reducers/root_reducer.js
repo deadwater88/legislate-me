abffce98454d16e4fff1c7f3240ef02ac4d0b2e8
Christine <christinegaribian@gmail.com>
2017-06-04 21:32:23 -0700

    Add subjects reducer to root reducer.

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import SubjectsReducer from './subjects_reducer';
 
 const RootReducer = combineReducers({
-
+  subjects: SubjectsReducer
 });
 
 export default RootReducer;
