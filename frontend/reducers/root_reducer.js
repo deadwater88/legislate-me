6b4420cd1cb1fe2df891f0cc05368b3c96c37fe1
Christine <christinegaribian@gmail.com>
2017-06-05 10:27:18 -0700

    Add subject reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import SubjectsReducer from './subjects_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
+  subjects: SubjectsReducer
 });
 
 export default RootReducer;
