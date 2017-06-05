fef7cdd81c151655d6b5648b916eab1078499bdd
Leland Krych <Leland.Krych@gmail.com>
2017-06-05 16:37:38 -0700

    Make login form toggle

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,46 +1,91 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 import { Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends Component {
 constructor(props){
   super(props);
   this.state = {email: '',
-                password: ''};
+                password: '',
+                login: true};
+  this.onPressButton = this.onPressButton.bind(this);
+}
+onPressButton(){
+  let toggle = !this.state.login;
+  this.setState({login: toggle});
 }
 
-
   render(){
-    return(
-      <Card>
-        <CardSection>
-          <Text> oAuth Icons, very clickable </Text>
-        </CardSection>
-        <CardSection>
-          <Input
-            placeholder="user@gmail.com"
-            label="Email"
-            value={this.state.email}
-            onChangeText={email =>this.setState({email})}
-             />
-        </CardSection>
+    if(this.state.login){
+      return(
+        <Card>
+          <CardSection>
+            <Text> oAuth Icons, very clickable </Text>
+          </CardSection>
+          <CardSection>
+            <Input
+              placeholder="user@gmail.com"
+              label="Email"
+              value={this.state.email}
+              onChangeText={email =>this.setState({email})}
+               />
+          </CardSection>
 
-        <CardSection >
-          <Input
-            secureTextEntry
-            placeholder="swedishfish42"
-            label="Password"
-            value={this.state.password}
-            onChangeText={password =>this.setState({password})}
-             />
-        </CardSection>
-        <CardSection>
-          <Button>
-            Log in
-          </Button>
-        </CardSection>
-      </Card>
-    );
+          <CardSection >
+            <Input
+              secureTextEntry
+              placeholder="swedishfish42"
+              label="Password"
+              value={this.state.password}
+              onChangeText={password =>this.setState({password})}
+               />
+          </CardSection>
+          <CardSection>
+            <Button>
+              Log in
+            </Button>
+          </CardSection>
+          <TouchableHighlight onPress={this.onPressButton}>
+              <Text>Or log in</Text>
+          </TouchableHighlight>
+        </Card>
+      );
+    }else{
+      return(
+        <Card>
+          <CardSection>
+            <Text> oAuth Icons, very clickable </Text>
+          </CardSection>
+          <CardSection>
+            <Input
+              placeholder="user@gmail.com"
+              label="Email"
+              value={this.state.email}
+              onChangeText={email =>this.setState({email})}
+               />
+          </CardSection>
+
+          <CardSection >
+            <Input
+              secureTextEntry
+              placeholder="password"
+              label="Password"
+              value={this.state.password}
+              onChangeText={password =>this.setState({password})}
+               />
+          </CardSection>
+          <CardSection>
+            <Button>
+              Sign Up!
+            </Button>
+          </CardSection>
+          <TouchableHighlight onPress={this.onPressButton}>
+              <Text>Or log in</Text>
+          </TouchableHighlight>
+        </Card>
+      );
+    }
+
   }
 }
 
