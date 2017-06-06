8acab3d35fab6f2dcbf35e5445c05a642a34b9ac
Christine <christinegaribian@gmail.com>
2017-06-05 21:45:56 -0700

    Allow user to swipe between splash, oauth, and regular login

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -19,46 +19,31 @@ import CustomizeInterestList from './CustomizeInterestList/CustomizeInterestList
 
 // Initialize three cards that the user can swipe between:
 // Splash Page, OAuth login, Normal Login
-
-
-// const LegislateMe = React.createClass({
-//   render: function(){
-//     return (
-//       <Swiper>
-//         <View>
-//           <Splash/>
-//         </View>
-//         <View>
-//           <LoginForm/>
-//         </View>
-//         <View>
-//           <CustomizeInterestList/>
-//         </View>
-//       </Swiper>
-//     )
-//   }
-// });
-
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
-  render() {
-    return <View>
-      <Text>Hello, Navigation!</Text>
-      <FBOAuth />
-      <GoogleSigninButton
-      style={{width: 48, height: 48}}
-      size={GoogleSigninButton.Size.Icon}
-      color={GoogleSigninButton.Color.Dark}
-      onPress={console.log("hi")}/>
-    </View>
+const LegislateMe = React.createClass({
+  render: function(){
+    return (
+      <Swiper>
+        <View>
+          <Splash/>
+        </View>
+        <View>
+          <Text>Hello, Navigation!</Text>
+          <FBOAuth />
+          <GoogleSigninButton
+          style={{width: 48, height: 48}}
+          size={GoogleSigninButton.Size.Icon}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={console.log("hi")}/>
+        </View>
+        <View>
+          <LoginForm/>
+        </View>
+        <View>
+          <CustomizeInterestList/>
+        </View>
+      </Swiper>
+    )
   }
-}
-
-const LegislateMe = StackNavigator({
-  Splash: { screen: Splash },
 });
-// Login: {screen: LoginForm }
 
 AppRegistry.registerComponent('legislate_me', () => LegislateMe);
