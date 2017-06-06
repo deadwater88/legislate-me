993692801b29c0728384e18648847812aae588c9
Leland Krych <Leland.Krych@gmail.com>
2017-06-06 11:55:03 -0700

    add errors for fetch representatives action creator

--- frontend/actions/representatives_actions.js
+++ frontend/actions/representatives_actions.js
@@ -1,20 +1,30 @@
 import * as APIUtil from '../util/user_api_util';
 
 export const RECEIVE_REPRESENTATIVES = 'RECEIVE_REPRESENTATIVES';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
 export const receiveRepresentatives= representatives => ({
   type: RECEIVE_REPRESENTATIVES,
   representatives
 });
 
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
 export const createAddress = (address) => dispatch => (
   APIUtil.createAddress(address).then(representatives =>
     dispatch(receiveRepresentatives(representatives)
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
   ))
 );
 
 export const updateAddress = (address) => dispatch => (
   APIUtil.updateAddress(address).then(representatives =>
     dispatch(receiveRepresentatives(representatives)
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
   ))
 );
