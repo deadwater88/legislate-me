e7f110563bacbc77590ee416fb19489cea0778b4
Leland Krych <Leland.Krych@gmail.com>
2017-06-06 11:46:00 -0700

    Finish RepresentativesReducer

--- frontend/reducers/representatives_reducer.js
+++ frontend/reducers/representatives_reducer.js
@@ -1,1 +1,18 @@
 import merge from 'lodash/merge';
+
+import {
+  RECEIVE_REPRESENTATIVES
+} from '../actions/representatives_actions';
+
+const RepresentativesReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState = merge({},state);
+  switch(action.type){
+    case RECEIVE_REPRESENTATIVES:
+      return action.representatives;
+    default:
+      return state;
+  }
+};
+
+export default RepresentativesReducer;
