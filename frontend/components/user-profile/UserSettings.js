52d7235f4d29ae4157196b1cce442f13ad425d1a
Leland Krych <Leland.Krych@gmail.com>
2017-06-06 10:49:52 -0700

    Create UserSettingsContainer

--- frontend/components/user-profile/UserSettings.js
+++ frontend/components/user-profile/UserSettings.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { Button, Card, CardSection } from '../common';
+import { Button, Card, CardSection, Input } from '../common';
+
+
 
 class UserSettings extends React.Component {
   constructor(props){
     super(props);
+    this.state = { address: ''};
+    this.signOut = this.signOut.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
+  }
+
+  signOut(){
+    this.props.signOut();
+  }
+
+  deleteUser(){
+    this.props.deleteUser();
   }
 
   render(){
     return(
       <Card>
+        <CardSection >
+          <Input
+            placeholder={this.props.currentUser.district}
+            label="Change your address"
+            value={this.state.address}
+            onChangeText={address =>this.setState({address})}
+             />
+        </CardSection>
         <CardSection>
-          <Button>
+          <Button onPress={this.props.signOut}>
             Sign out
           </Button>
         </CardSection>
         <CardSection>
-          <Button>
+          <Button onPress={this.props.deleteUser}>
             Delete your account
           </Button>
         </CardSection>
---
+++ frontend/components/user-profile/UserSettingsContainer.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+
+import UserSettings from './UserSettings';
+
+const mapStateToProps = state => (
+  {
+
+  }
+);
+
+const mapDispatchToProps = dispatch => (
+  {
+
+  }
+);
+
+export default connect(mapStateToProps,mapDispatchToProps)(UserSettings);
