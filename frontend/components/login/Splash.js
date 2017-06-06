45d1c238a6f7788566dc5ce230c67d54035c6621
Christine <christinegaribian@gmail.com>
2017-06-05 22:54:02 -0700

    remove console logs

---
+++ frontend/components/login/Splash.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
+import { Card, CardSection, Button } from '../common';
+import { StackNavigator } from 'react-navigation';
+
+class Splash extends Component {
+  constructor(props){
+    super(props);
+    console.log(props);
+  }
+  render(){
+    const { headerStyle, imageStyle} = styles;
+    const pic  = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    const navigate = this.props.navigation;
+
+    return (
+      <Card>
+        <CardSection>
+          <Text style={headerStyle}> Now is the time for action. Make your voice heard </Text>
+        </CardSection>
+        <CardSection>
+          <Image style={imageStyle} source={pic}/>
+        </CardSection>
+        <CardSection>
+          <Button
+            onPress={() => {
+              navigate('Login');
+            }}>
+            Get Started
+          </Button>
+        </CardSection>
+
+      </Card>
+    )
+  }
+};
+
+const styles = {
+  headerStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 1
+  },
+  imageStyle: {
+    flex: 2,
+    width: 100,
+    height: 100
+  }
+};
+
+export default Splash;
