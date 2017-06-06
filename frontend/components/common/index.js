32ea771637e87624d937a06f76a3795d2060179b
Leland Krych <Leland.Krych@gmail.com>
2017-06-06 09:59:19 -0700

    Add documentation to common components and start UserSettings component

--- frontend/components/common/index.js
+++ frontend/components/common/index.js
@@ -1,4 +1,9 @@
+//reusable button component, takes argument for button label
 export * from './Button';
+//reusable background component, you should only have one card per page.
 export * from './Card';
+//reusable nesting component for card component, use these like you
+// would divs. Check out myLogInForm if you have any questions!
 export * from './CardSection';
+//reusable text input component
 export * from './Input';
