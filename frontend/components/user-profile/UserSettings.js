f512b405b064b54ba05f3f7a3a899ea9fdaff7fe
Leland Krych <Leland.Krych@gmail.com>
2017-06-06 10:39:29 -0700

    Add base for usersettings

--- frontend/components/user-profile/UserSettings.js
+++ frontend/components/user-profile/UserSettings.js
@@ -1,3 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { Button, Card, CardSection } from '../common';
+
+class UserSettings extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <Card>
+        <CardSection>
+          <Button>
+            Sign out
+          </Button>
+        </CardSection>
+        <CardSection>
+          <Button>
+            Delete your account
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default UserSettings;
