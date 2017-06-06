587963a27a235e3d1cd2abbf75b4437d2450bce3
Leland Krych <Leland.Krych@gmail.com>
2017-06-06 11:00:22 -0700

    Create UserAPIUtil

--- frontend/components/user-profile/UserSettingsContainer.js
+++ frontend/components/user-profile/UserSettingsContainer.js
@@ -1,16 +1,18 @@
 import { connect } from 'react-redux';
 
 import UserSettings from './UserSettings';
+import { logout } from '../../actions/session_actions';
 
 const mapStateToProps = state => (
   {
-
+    session: state.session
   }
 );
 
 const mapDispatchToProps = dispatch => (
   {
-
+    logout: user => dispatch(logout(user)),
+    deleteUser: user => dispatch(deleteUser(user))
   }
 );
 
