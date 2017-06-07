14931aec17ae1acb80f5b5e277dc3052b997eb13
Christine <christinegaribian@gmail.com>
2017-06-07 10:46:13 -0700

    Set up font awesome icons

--- frontend/components/common/navbar/navbar.js
+++ frontend/components/common/navbar/navbar.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Navbar extends Component {
   constructor(props){
     super(props);
+    this.goToProfile = this.goToProfile.bind(this);
+    this.goToHome = this.goToHome.bind(this);
+    this.goToBookmarks = this.goToBookmarks.bind(this);
+    this.goToExplore = this.goToExplore.bind(this);
   }
 
   render(){
-
+    const {buttonStyle} = styles;
     return (
       <View>
-        <Text> Navbar  </Text>
+        <TouchableOpacity onPress={this.goToProfile} style={buttonStyle}>
+        <Icon.Button name="home" backgroundColor="#3b5998" onPress={this.loginWithFacebook}>
+          Login with Facebook
+        </Icon.Button>
+        </TouchableOpacity>
       </View>
     )
   }
+
+  goToProfile(){
+    console.log("Profile");
+  }
+  goToHome(){
+    console.log("Home");
+  }
+  goToDiscover(){
+    console.log("Discover");
+  }
+  goToBookmarks(){
+    console.log("Bookmarks");
+  }
 }
+
+
+const styles = {
+  buttonStyle: {
+
+  }
+};
 
 export default Navbar;
