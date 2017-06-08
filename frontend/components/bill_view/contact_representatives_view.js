280781efceb575a6e239e20865bf51b41a141632
Leland Krych <Leland.Krych@gmail.com>
2017-06-07 19:25:10 -0700

    Add callback functions for icons

--- frontend/components/bill_view/contact_representatives_view.js
+++ frontend/components/bill_view/contact_representatives_view.js
@@ -3,31 +3,56 @@ import { Text, TouchableOpacity } from 'react-native';
 import { CardSection } from '../common';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const ContactRepresentativesView = ({reps}) => {
-  const styles = {
-    container: {
-                display: 'flex',
-                flexDirection: 'row',
-              },
-    contact: {
+class ContactRepresentativesView extends React.Component {
+   constructor(props){
+     super(props);
+     this.onEmail = this.onEmail.bind(this);
+     this.onCall = this.onCall.bind(this);
+   }
 
-    },
-    phonecall: {
+   onEmail(){
 
-    },
-    email: {
+   }
 
-    }
-  };
-  const { container, contact, phoneCall, email } = styles;
+   onCall(){
 
-  return(
-    <CardSection style={container}>
-      <Text style={contact}>Contact your representatives</Text>
-      <Icon name="phone-square" size={30} color="#009E11" style={phoneCall} />
-      <Icon name="envelope" size={30} color="#CF2A28" style={email} />
-    </CardSection>
-  );
+   }
+   
+   render(){
+
+     const { container, contact, phoneCall, email } = styles;
+
+     return(
+       <CardSection style={container}>
+         <Text style={contact}>Contact your representatives</Text>
+         <Icon name="phone-square"
+            size={30}
+            color="#009E11"
+            onPress={this.onCall}
+            style={phoneCall} />
+         <Icon name="envelope"
+            size={30}
+            color="#CF2A28"
+            onPress={this.onEmail}
+            style={email} />
+       </CardSection>
+     );
+   }
+}
+const styles = {
+  container: {
+              display: 'flex',
+              flexDirection: 'row',
+            },
+  contact: {
+
+  },
+  phonecall: {
+
+  },
+  email: {
+
+  }
 };
 
 export default ContactRepresentativesView;
