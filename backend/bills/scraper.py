1f0edf5c77a098e776fac2e60c5c34e0cb0975c0
Christine <christinegaribian@gmail.com>
2017-06-08 09:40:59 -0700

    Replace all html tags in the digest text with empty string

--- backend/bills/scraper.py
+++ backend/bills/scraper.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import urllib
 
-def bill_scraper(bill_url):
+def blurb_scraper(bill_url):
     r = urllib.urlopen(bill_url).read()
     soup = BeautifulSoup(r)
     blurb = soup.find(id="digesttext")
 
-    for tag in soup.find_all('strong'):
+    for tag in blurb.find_all('strong'):
         tag.replaceWith('')
 
     return blurb.get_text()
