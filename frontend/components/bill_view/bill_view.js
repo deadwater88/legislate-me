414bb14c60109bce8645945b273a66adf8f7c3b0
Leland Krych <Leland.Krych@gmail.com>
2017-06-07 18:29:59 -0700

    Add more skeleton to BillView

--- frontend/components/bill_view/bill_view.js
+++ frontend/components/bill_view/bill_view.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import {Text, View, TouchableOpacity } from 'react-native';
 
-import { Card, CardSection } from '../common';
+import contactRepresentativesView  from './contactRepresentativesView';
+import legislatorBillView from './legislatorBillView';
 
+import { Card, CardSection } from '../common';
+//Pass Bill Info in with props as bill
 class BillView extends React.Component {
   constructor(props){
+    super(props);
+    this.state = {
+                  reveal_blurb: false
+                };
+    this.toggleBlurb = this.toggleBlurb.bind(this);
+  }
 
+  toggleBlurb(){
+    const newState = !this.state.reveal_blurb;
+    this.setState({reveal_blurb: newState});
   }
 
   render(){
+    const {billTitle, billImage, blurb  } = styles;
+
     return(
-      <Text>Hello World!</Text>
+      <Card>
+        <CardSection>
+          <Text style={billTitle}>{this.props.bill.title}</Text>
+        </CardSection>
+        <CardSection>
+        </CardSection>
+        <CardSection>
+        </CardSection>
+      </Card>
     );
   }
 }
