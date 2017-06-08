d5caa347a021041eeffe0b6d74ee380d8e931a5e
Leland Krych <Leland.Krych@gmail.com>
2017-06-07 18:36:13 -0700

    Add Chamber and state to BillView

--- frontend/components/bill_view/bill_view.js
+++ frontend/components/bill_view/bill_view.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Text, View, TouchableOpacity } from 'react-native';
 
-import contactRepresentativesView  from './contactRepresentativesView';
-import legislatorBillView from './legislatorBillView';
+import ContactRepresentativesView  from './contactRepresentativesView';
+import LegislatorBillView from './legislatorBillView';
 
 import { Card, CardSection } from '../common';
 //Pass Bill Info in with props as bill
@@ -21,20 +21,52 @@ class BillView extends React.Component {
   }
 
   render(){
-    const {billTitle, billImage, blurb  } = styles;
-
+    const {billTitle, billImage, blurb, billState, billChamber } = styles;
+    const pic  = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Golden_Gate_bridge_pillar.jpg/400px-Golden_Gate_bridge_pillar.jpg'
+    };
     return(
       <Card>
         <CardSection>
           <Text style={billTitle}>{this.props.bill.title}</Text>
+          <Text style={billState}>{this.props.bill.state}</Text>
+          <Text style={billChamber}>{this.props.bill.chamber}</Text>
         </CardSection>
         <CardSection>
+            <Image style={billImage} source={pic} />
         </CardSection>
         <CardSection>
+          <ContactRepresentativesView
+            reps={this.props.contactRepresentatives} />
+        </CardSection>
+        <CardSection>
+          <Text style={blurb}>blurby</Text>
+        </CardSection>
+        <CardSection>
+          <LegislatorBillView
+            legislators={this.props.bill.sponsors} />
         </CardSection>
       </Card>
     );
   }
 }
+const styles = {
+
+    billTitle: {
+
+    },
+    billImage: {
+
+    },
+    blurb: {
+
+    },
+    billState: {
+
+    },
+    billChamber: {
+
+    }
+};
 
 export default BillView;
