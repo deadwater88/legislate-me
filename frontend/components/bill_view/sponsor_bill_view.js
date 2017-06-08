86f4708c68e4050041a759c79ffb0ff81d79a206
Leland Krych <Leland.Krych@gmail.com>
2017-06-07 19:28:29 -0700

    Mockout SponsorBillView

--- frontend/components/bill_view/sponsor_bill_view.js
+++ frontend/components/bill_view/sponsor_bill_view.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import RepresentativesView from '../user_profile/RepresentativesView';
+
+const SponsorBillView = ({sponsors}) => {
+  return(
+    <RepresentativesView reps={sponsors}/>
+  );
+};
+
+export default SponsorBillView;
