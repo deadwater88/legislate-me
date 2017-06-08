aa83c507c73c805a676ac575238e0ff28dba874a
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-06-07 20:42:39 -0700

    Navigate to home after login

--- frontend/components/bill_index/bill_index.js
+++ frontend/components/bill_index/bill_index.js
@@ -3,28 +3,24 @@ import { Text, ListView, Image } from 'react-native';
 class BillIndex extends React.Component{
   constructor(props){
     super(props);
-    //this.props.bills will be an array
-    //of pojos
-    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: dataSource.cloneWithRows(this.props.bills)
-    };
-    this.bookmarkBill = this.bookmarkBill.bind(this);
-    //average of 200wpm
-    //get the count of the bill after scraping it
-    // and display bill read time
+    //this.props.bills will be an array of pojos
+
+    //temporarily overriding
+    this.props.bills = [{}, {}, {}];
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    // this.state = {
+    //   dataSource: ds.cloneWithRows(this.props.bills)
+    // };
   }
 
-
   render(){
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={ bill => <BillIndexItem
-          bill={bill}
-          bookmarkBill={this.props.bookmarkBill} />}
-       />
-   );
+    // <ListView
+    //   dataSource={this.state.dataSource}
+    //   renderRow={ bill => <BillIndexItem
+    //     bill={bill}
+    //     bookmarkBill={this.props.bookmarkBill} />}
+    //     />
+    return (<Text></Text>);
   }
 }
 
