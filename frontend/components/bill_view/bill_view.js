cbc2d1d75df908c7505f17bbc8a098a968870a8b
Leland Krych <Leland.Krych@gmail.com>
2017-06-07 18:18:49 -0700

    Finish skeleton BillView

---
+++ frontend/components/bill_view/bill_view.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View, TouchableOpacity } from 'react-native';
+
+import { Card, CardSection } from '../common';
+
+class BillView extends React.Component {
+  constructor(props){
+
+  }
+
+  render(){
+    return(
+      <Text>Hello World!</Text>
+    );
+  }
+}
+
+export default BillView;
