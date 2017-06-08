27677a69ad181e58f40d313f2988c33678c70c74
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-06-08 09:39:32 -0700

    Add scraper.py

---
+++ backend/bills/scraper.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import urllib
+
+def blurb_scraper(bill_url):
+    r = urllib.urlopen(bill_url).read()
+    soup = BeautifulSoup(r, "lxml")
+    soup = soup.find("span", {"id": "digesttext"})
+    text = soup.get_text()
+    return text
+print blurb_scraper('http://leginfo.legislature.ca.gov/faces/billNavClient.xhtml?bill_id=201720180SB6')
