3dd8485c747e7f9fe830dd7837a44662f875c8e0
Leland Krych <Leland.Krych@gmail.com>
2017-06-08 20:46:04 -0700

    Create Skeleton for Subjects_Index

---
+++ frontend/components/subjects/subjects_index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+
+class SubjectsIndex extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+  }
+}
+
+const styles = {
+
+};
+
+export default SubjectsIndex;
