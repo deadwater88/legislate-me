ab3e44607eaf4507ac1ac39bcb6032fe5b0a2399
Leland Krych <Leland.Krych@gmail.com>
2017-06-08 21:04:06 -0700

    Add map logic to SubjectsIndex

--- frontend/components/subjects/subjects_index.js
+++ frontend/components/subjects/subjects_index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableHighlight, ScrollView } from 'react-native';
 
-import { SUBJECTS } from '../util/subject_api_util';
+import { SUBJECTS, SUBJECT_IMAGES } from '../util/subject_api_util';
 
 class SubjectsIndex extends React.Component {
   constructor(props){
@@ -9,16 +9,27 @@ class SubjectsIndex extends React.Component {
   }
 
   render(){
-    
+    const { imageStyle } = styles;
+    const subjects = SUBJECTS.keys().map(key => {
+      return(
+        <View style={{flexDirection: 'column'}}>
+          <Image style={imageStyle} source={SUBJECT_IMAGES[`${key}`]} />
+          <Text>key</Text>
+        </View>
+      );
+    });
     return(
       <ScrollView>
+        <Text style={{}}>Discover new bills by subject</Text>
       </ScrollView>
     );
   }
 }
 
 const styles = {
+  imageStyle: {
 
+  }
 };
 
 export default SubjectsIndex;
