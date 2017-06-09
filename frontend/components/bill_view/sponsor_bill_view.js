40695759643969faa5b074b2f1da64718c76af34
Leland Krych <Leland.Krych@gmail.com>
2017-06-08 20:16:37 -0700

    refactor sponsor_bill_view

--- frontend/components/bill_view/sponsor_bill_view.js
+++ frontend/components/bill_view/sponsor_bill_view.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import RepresentativesView from '../user_profile/RepresentativesView';
+import {Image, Text, View, Linking} from 'react-native';
 
-const SponsorBillView = ({sponsors}) => {
+const SponsorBillView = ({sponsor}) => {
+  const { sponsorName, sponsorImage } = styles;
+  const pic  = {
+    uri: 'http://dccouncil.us/files/user_uploads/member_photos/barry.jpg'
+  };
   return(
-    <RepresentativesView reps={sponsors}/>
+    <View style={{flexDirection: 'row'}}>
+      <View style={{flex: 1}}>
+        <Image style={sponsorImage} source={pic} />
+      </View>
+      <View style={{flexDirection: 'column', flex: 1}}>
+        <Text style={sponsorName}> {sponsor.name}</Text>
+        <Text > {sponsor.role}</Text>
+        <Text > {sponsor.party}</Text>
+          <Text style={{color: 'blue'}}
+          onPress={() => Linking.openURL(`${sponsor.website}`)}>
+          Official Website
+        </Text>
+      </View>
+    </View>
   );
 };
 
+const styles = {
+  sponsorName: {
+  },
+  sponsorImage: {
+    height: 100,
+    borderRadius: 50,
+    width: 100
+  }
+};
 export default SponsorBillView;
