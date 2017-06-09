6b02a0251319259fb41c884cd6f8eb4aeed424dc
Leland Krych <Leland.Krych@gmail.com>
2017-06-09 11:50:53 -0700

    Add AJAX to subject_contaier

--- frontend/components/subjects/subject_container.js
+++ frontend/components/subjects/subject_container.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 
 import Subject from './subject';
-import { fetchSubject } from '../util/subject_api_util'; 
+import { fetchSubject } from '../util/subject_api_util';
 
-const mapDispatchToProps =  ({
-
+const mapDispatchToProps = dispatch =>  ({
+  fetchSubject: subject => dispatch(fetchSubject(subject))
 });
 
 
