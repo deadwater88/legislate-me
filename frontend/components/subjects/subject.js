34e944e4c428397aae2097ac99b5539dee2263c6
Leland Krych <Leland.Krych@gmail.com>
2017-06-09 11:54:48 -0700

    add ComponentDidMount

--- frontend/components/subjects/subject.js
+++ frontend/components/subjects/subject.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
 
-class Subject extends React.Component {
 
+class Subject extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+componentDidMount(){
+  this.props.fetchSubject();
 }
 
+  render(){
+    return(
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  header: {
+
+  }
+};
 
 export default Subject;
