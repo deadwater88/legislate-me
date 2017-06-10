a0458a956b9d51e2f6027a7a9cf3a51bfb24dbe1
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-06-10 16:24:29 -0700

    Remove red error from clicking on find-your-rep

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -42,16 +42,15 @@ class LoginSwiping extends Component{
   render(){
     const navigate = this.props.navigation.navigate;
 
-    // <Swiper>
-    //   <View>
-    //     <Splash navigation={this.props.navigation} />
-    //   </View>
-    //   <View>
-    //     <LoginNavigator />
-    //   </View>
-    // </Swiper>
     return (
-      <SubjectsIndex />
+      <Swiper>
+        <View>
+          <Splash navigation={this.props.navigation} />
+        </View>
+        <View>
+          <LoginNavigator />
+        </View>
+      </Swiper>
     );
   }
 }
