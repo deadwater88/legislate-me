f3c753c486e5e2c5a6125fe569097017f6820bf4
Leland Krych <Leland.Krych@gmail.com>
2017-06-10 22:30:13 -0700

    Refactor representatives_reducer

--- frontend/reducers/representatives_reducer.js
+++ frontend/reducers/representatives_reducer.js
@@ -1,7 +1,7 @@
 import merge from 'lodash/merge';
 
+import  { RECEIVE_CURRENT_USER }
 import {
-  RECEIVE_REPRESENTATIVES,
   RECEIVE_ERRORS
 } from '../actions/representatives_actions';
 
@@ -14,9 +14,14 @@ const RepresentativesReducer = (state = nullReps, action) => {
   Object.freeze(state);
   let newState = merge({},state);
   switch(action.type){
+    case RECEIVE_CURRENT_USER:
+      const newReps = action.currentUser.representatives;
+      //imgUrl, fName, lName, email, phNum
+      //newReps is an array of rep objects fName, lName,
+      return merge({}, nullReps, { currentReps: newReps });
     case RECEIVE_REPRESENTATIVES:
       const newReps = action.representatives;
-      //imgUrl, fName, lName, email, phNum 
+      //imgUrl, fName, lName, email, phNum
       //newReps is an array of rep objects fName, lName,
       return merge({}, nullReps, { currentReps: newReps }); //overrride currentReps property of nullReps
     case RECEIVE_ERRORS:
