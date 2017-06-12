f53039a1d84725cec763e5cedc9af4ed2dfec2d8
David Wong <david.wong.cal@gmail.com>
2017-06-12 00:20:57 -0700

    prep for deployment

--- backend/bills/urls.py
+++ backend/bills/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'subjects/([A-Za-z0-9_-]+)', views.BillsbySubjectView.as_view()),
+    url('subjects/([A-Za-z0-9\s]+)', views.BillsbySubjectView.as_view()),
     url(r'bookmarked', views.BookmarkedBillsView.as_view()),
     url(r'([A-Za-z0-9_-]+)', views.BillView.as_view()),
     url(r'^$', views.BillsView.as_view()),
