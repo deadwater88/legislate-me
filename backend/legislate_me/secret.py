50cc9d7f8385bf90b2106a461ca0da6d605aeb19
David Wong <david.wong.cal@gmail.com>
2017-07-11 20:06:51 -0700

    Remove secret.py from git respostiory

--- backend/legislate_me/secret.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'o$^vfdbwo485il962it%scmcje^g)!&(c=43&b8p87seiw5t9s'
--- backend/legislate_me/settings.py
+++ backend/legislate_me/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-from legislate_me.secret import SECRET_KEY
+from legislate_me.secret import SECRET_KEY, DATABASE_CREDENTIALS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,16 +84,7 @@ AUTH_USER_MODEL = 'users.User'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'legislate_me_dev',
-        'USER': 'lmadmin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+DATABASES = DATABASE_CREDENTIALS
 
 
 # Password validation
