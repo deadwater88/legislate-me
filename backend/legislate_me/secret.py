1e28376bfb667ede3ed3f67fac48ceacbf04ad1d
David Wong <david.wong.cal@gmail.com>
2017-07-11 20:12:55 -0700

    transfer database credentials to secret.py

--- backend/legislate_me/secret.py
+++ backend/legislate_me/secret.py
@@ -1,1 +1,12 @@
 SECRET_KEY = 'o$^vfdbwo485il962it%scmcje^g)!&(c=43&b8p87seiw5t9s'
+
+DATABASE_CREDENTIALS = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'legislate_me_dev',
+        'USER': 'lmadmin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
